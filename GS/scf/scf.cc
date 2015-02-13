a9d7a74f64f132982f59f121753a4156da6bca13
kannon92 <kevinhannon@Kevins-MacBook-Pro.local>
2015-02-13 16:14:55 -0500

    Added SCF

--- GS/scf/scf.cc
+++ GS/scf/scf.cc
@@ -27,11 +27,13 @@
 #include <libparallel/parallel.h>
 #include <liboptions/liboptions.h>
 #include <libmints/mints.h>
+#include <libmints/vector.h>
 #include <libpsio/psio.h>
+#include "scf.h"
 
 INIT_PLUGIN
-
 namespace psi{ namespace scf {
+
 
 extern "C"
 int read_options(std::string name, Options &options)
@@ -42,13 +44,32 @@ int read_options(std::string name, Options &options)
         options.add_int("PRINT", 1);
         /*- Whether to compute two-electron integrals -*/
         options.add_bool("DO_TEI", true);
+        options.add_bool("DO_FTHF", false);
     }
 
     return true;
 }
+Scfclass::Scfclass(boost::shared_ptr<Wavefunction> ref_wfn, Options& options) : Wavefunction(options, _default_psio_lib_)
+{
+    copy(ref_wfn);
+}
 
 extern "C"
 PsiReturnType scf(Options &options)
+{
+     boost::shared_ptr<Scfclass> run_scf(new 
+         Scfclass(Process::environment.wavefunction(), options));
+     run_scf->ao_ints(options); 
+     run_scf->scf_iteration();
+     
+
+    return Success;
+
+}
+Scfclass::~Scfclass()
+{
+}
+void Scfclass::ao_ints(Options& options)
 {
     int print = options.get_int("PRINT");
     int doTei = options.get_bool("DO_TEI");
@@ -91,6 +112,7 @@ PsiReturnType scf(Options &options)
     vOBI->compute(vMat);
 
     sMat->print();
+    sMat_= sMat->clone();
     tMat->print();
     vMat->print();
 
@@ -98,9 +120,11 @@ PsiReturnType scf(Options &options)
     hMat->copy(tMat);
     hMat->add(vMat);
     hMat->print();
+    hMat_ = hMat->clone();
+    nbf_  = nbf[0];
 
+    teiV_.reserve(nbf[0]*nbf[0]*nbf[0]*nbf[0]);
     if(doTei){
-         psi::outfile->Printf("\n  Two-electron Integrals\n\n");
 
         // Now, the two-electron integrals
         boost::shared_ptr<TwoBodyAOInt> eri(integral->eri());
@@ -119,16 +143,142 @@ PsiReturnType scf(Options &options)
                     int q = intIter.j();
                     int r = intIter.k();
                     int s = intIter.l();
-                    psi::outfile->Printf("\t(%2d %2d | %2d %2d) = %20.15f\n",
-                        p, q, r, s, buffer[intIter.index()]);
-                    ++count;
+                    teiV_[INDEX4(p,q,r,s)]=buffer[intIter.index()];
+                    
+
                 }
             }
         }
         psi::outfile->Printf("\n\tThere are %d unique integrals\n\n", count);
     }
+    
 
-    return Success;
+}
+void Scfclass::scf_iteration()
+{
+    boost::shared_ptr<Wavefunction> wfn = Process::environment.wavefunction();
+    int nocc = wfn->nalpha();
+    nocc_ = nocc;
+    outfile->Printf("\n nocc = %d", nocc);
+    boost::shared_ptr<Matrix> Evec(new Matrix("Evectors", nbf_, nbf_));
+    boost::shared_ptr<Vector> Eval(new Vector("Evals", nbf_));
+   
+    boost::shared_ptr<Matrix> Shalf(new Matrix("S^-(1/2)", nbf_, nbf_));
+    boost::shared_ptr<Matrix> Svec(new Matrix("Evectors", nbf_, nbf_));
+    boost::shared_ptr<Vector> Sval(new Vector("Evals", nbf_));
+    boost::shared_ptr<Matrix> SI(new Matrix("SI", nbf_, nbf_));
+    sMat_->diagonalize(Svec,Sval);
+    for(int mu = 0; mu < nbf_; mu++){
+       for(int nu = 0; nu < nbf_; nu++){
+          int factor = mu==nu ? 1 : 0;
+          SI->set(mu,nu,1.0/(sqrt(Sval->get(nu)))*factor);
+       }
+    }
+    SI->print();
+    for(int mu = 0; mu < nbf_; mu++){
+       for(int nu = 0; nu < nbf_; nu++){
+          for(int l = 0; l < nbf_; l++){
+             for(int s = 0; s < nbf_; s++){
+          Shalf->add(mu,nu,Svec->get(mu,l)*SI->get(l,s)*Svec->get(nu,s));
+             }
+          }
+       } 
+    } 
+    Shalf->print();
+    
+    boost::shared_ptr<Matrix> Fold(new Matrix("Fock", nbf_, nbf_));
+    Fold->copy(hMat_);
+    Fold->transform(Shalf);
+    Fold->print();
+    
+    SharedMatrix Fnotrans(new Matrix("Fnotrans", nbf_, nbf_)); 
+    Fold->diagonalize(Evec,Eval);
+ 
+    boost::shared_ptr<Matrix> C(new Matrix("C", nbf_, nbf_));
+    boost::shared_ptr<Vector> eps(new Vector("epsilon", nbf_));
+
+    C->gemm('F','F', 1.0, Shalf, Evec, 0.0);
+    C->print();
+
+    boost::shared_ptr<Matrix> D(new Matrix("D", nbf_,nbf_));
+    boost::shared_ptr<Matrix> Dtest(new Matrix("Dtest", nbf_, nbf_));
+   
+    for(int mu = 0; mu < nbf_; mu++){
+       for(int nu = 0; nu < nbf_; nu++){
+          for(int i = 0; i < nocc; i++){
+             Dtest->add(mu,nu,C->get(mu,i)*C->get(nu,i)); 
+          }
+       }
+    } 
+    if(options_.get_bool("DO_FTHF")==true){ 
+      frac_occupation(C,Eval,0);
+    }
+    else{
+      D->gemm('N','T',nbf_,nbf_,nocc, 1.0,C, nbf_, C, nbf_, 0, nbf_, 0,0,0);
+    }
+
+
+    boost::shared_ptr<Matrix> FpH(new Matrix("FpH", nbf_,nbf_)); 
+    FpH->copy(hMat_);
+    FpH->add(hMat_);
+
+    double energy_old = D->vector_dot(FpH);
+    boost::shared_ptr<Molecule> mol = Process::environment.molecule();
+    double nuc_rep = mol->nuclear_repulsion_energy();
+    outfile->Printf("\nNuc Replusion = %20.12f", nuc_rep);
+    outfile->Printf("\n 1st iteration = %20.12f", energy_old);
+    int iter = 0;
+    while(iter < 40)
+    {
+        energy_old = energy_;
+       
+        for(int mu = 0; mu < nbf_; mu++){
+           for(int nu = 0; nu < nbf_; nu++){
+              Fold->set(mu,nu,hMat_->get(mu,nu));
+              for(int lam = 0; lam < nbf_; lam++){
+                 for(int sig = 0; sig < nbf_; sig++){
+                    Fold->add(mu,nu,D->get(lam,sig) * (2.0*teiV_[INDEX4(mu,nu,lam,sig)] - teiV_[INDEX4(mu,lam,nu,sig)]));
+                 }
+              }
+           }
+        }
+        if(iter==0){Fold->print();}
+        Fnotrans->copy(Fold);
+        Fold->transform(Shalf); 
+        Evec->zero();
+        Eval->zero();
+        Fold->diagonalize(Evec,Eval);
+        C->zero();
+        C->gemm(false,false, 1.0, Shalf, Evec,0.0);
+
+      
+        if(options_.get_bool("DO_FTHF")==true){
+          frac_occupation(C,Eval, iter);
+        }
+        else{
+          D->gemm('n','t',nbf_,nbf_,nocc, 1.0,C, nbf_, C, nbf_, 0, nbf_, 0,0,0);
+        }
+        
+        FpH->copy(hMat_);
+        FpH->add(Fnotrans);
+        energy_ = D->vector_dot(FpH);
+   
+        iter++;
+  
+        outfile->Printf("\n iter:%d  energy:%20.12f  total:%20.12f  error:%20.12f", iter, energy_, energy_ + nuc_rep, energy_ - energy_old);
+    }
+    
+   
+
+}
+double Scfclass::compute_energy()
+{
+   return 0.0;
+}
+void Scfclass::frac_occupation(SharedMatrix C, SharedVector e,int iter)
+{
+
+
 }
 
 }} // End Namespaces
---
+++ GS/scf/scf.h
@@ -0,0 +1,33 @@
+#include <psi4-dec.h>
+#include <liboptions/liboptions.h>
+#include <libmints/mints.h>
+#include <libmints/wavefunction.h>
+
+#ifndef Embed_H
+#define Embed_H
+
+namespace psi { namespace scf{
+class Scfclass : public Wavefunction
+{
+protected:
+      SharedMatrix hMat_;
+      SharedMatrix sMat_;
+      std::vector<double> teiV_;
+      int nbf_;
+      int nmo_;
+      int nocc_;
+      double compute_energy();
+      void frac_occupation(SharedMatrix, SharedVector, int);
+      double energy = 0.0;
+private:
+public:
+      void scf_energy();
+      void scf_iteration();
+      Scfclass(boost::shared_ptr<Wavefunction> wfn, Options& Options);
+      void ao_ints(Options& options);
+      virtual ~Scfclass();
+};
+
+}}
+#endif /* !EMBED_H */
+
